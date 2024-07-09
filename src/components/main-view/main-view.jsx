dd9b2d08099ec8c3ba31efb8a21441d56924eb2e
Monica Alyssa <monica_alyssa@icloud.com>
2024-07-08 20:50:03 -0400

    Update movie array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import React from "react";
-import { MovieCard } from "../movie-view";
+import { MovieCard } from "../movie-card";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([
-    { id: 1, title: "Avatar: The Way of Water" },
-    { id: 2, title: "The Maze Runner" },
-    { id: 3, title: "Divergent" }
+    {
+      id: 1,
+      title: "Avatar: The Way of Water",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_.jpg",
+      director: "James Cameron"
+    },
+    {
+      id: 2,
+      title: "The Maze Runner",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMjUyNTA3MTAyM15BMl5BanBnXkFtZTgwOTEyMTkyMjE@._V1_.jpg",
+      director: "Wes Ball"
+    },
+    {
+      id: 3,
+      title: "Divergent",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMTYxMzYwODE4OV5BMl5BanBnXkFtZTgwNDE5MzE2MDE@._V1_FMjpg_UY2048_.jpg",
+      director: "Neil Burger"
+    }
   ]);
 
   if (movies.length === 0) {
@@ -16,7 +34,7 @@ export const MainView = () => {
   return (
     <div>
       {movies.map((movieprop) => {
-        <MovieCard movieprop={movies}/>
+        <MovieCard movieprop={movies} />;
       })}
     </div>
   );
